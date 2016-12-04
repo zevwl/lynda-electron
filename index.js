1a272a1f1a9a4dbf541fb88f57f3bd595cd8e3fb
zevwl <zevwl@bhphoto.com>
2016-12-03 21:22:37 -0500

    Managing window events

--- index.js
+++ index.js
@@ -4,16 +4,27 @@ const app = electron.app;
 
 app.on('ready', () => {
 
-    let appWindow = new BrowserWindow();
+    let appWindow = new BrowserWindow({
+        show: false
+    });
     appWindow.loadURL('https://zevwl.github.io');
 
     let infoWindow = new BrowserWindow({
         width: 400,
         height: 300,
+        show: false,
         transparent: true,
         frame: false
     });
 
     infoWindow.setMenu(null);
     infoWindow.loadURL(`file://${__dirname}/info.html`);
+
+    appWindow.once('ready-to-show', () => {
+        appWindow.show();
+        setTimeout(() => {
+            infoWindow.show();
+            setTimeout(() => infoWindow.hide(), 3000);
+        }, 1000);
+    });
 });
